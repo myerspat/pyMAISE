678aa8e6b11f94c00fdefdeb17c4af4ca2f42ff6
myerspat <myerspat@umich.edu>
2023-11-08 10:26:11 -0500

    Refactored PreProcessor using xarray

--- pyMAISE/data/_handler.py
+++ pyMAISE/data/_handler.py
@@ -1,6 +1,6 @@
 from pkg_resources import resource_filename
 
-from pyMAISE.preprocessing import PreProcesser
+from pyMAISE.preprocessing import PreProcessor
 
 
 # Get full pyMAISE data file path
@@ -10,51 +10,67 @@ def get_full_path(path: str):
 
 # Load benchmark BWR cross section data
 def load_xs():
-    return PreProcesser(get_full_path("data/xs.csv"), slice(0, -1), slice(-1, None))
+    preprocessor = PreProcessor()
+    preprocessor.read_csv(get_full_path("data/xs.csv"), slice(0, -1), slice(-1, None))
+    return preprocessor
 
 
 # Load benchmark MIT reactor data
 def load_MITR():
-    return PreProcesser(
+    preprocessor = PreProcessor()
+    preprocessor.read_csv(
         [get_full_path("data/crx.csv"), get_full_path("data/powery.csv")],
     )
+    return preprocessor
 
 
 # Load benchmark fuel perfromance data
 def load_fp():
-    return PreProcesser(
+    preprocessor = PreProcessor()
+    preprocessor.read_csv(
         [get_full_path("data/fp_inp.csv"), get_full_path("data/fp_out.csv")]
     )
+    return preprocessor
 
 
 # Load benchmark fuel centerline temperature data
 def load_heat():
-    return PreProcesser(get_full_path("data/heat.csv"), slice(0, -1), slice(-1, None))
+    preprocessor = PreProcessor()
+    preprocessor.read_csv(get_full_path("data/heat.csv"), slice(0, -1), slice(-1, None))
+    return preprocessor
 
 
 # Rod ejection accident data
 def load_rea():
-    return PreProcesser(
+    preprocessor = PreProcessor()
+    preprocessor.read_csv(
         [get_full_path("data/rea_inputs.csv"), get_full_path("data/rea_outputs.csv")],
     )
+    return preprocessor
 
 
 # Load BWR micro-reactor data
 def load_BWR():
-    return PreProcesser(
+    preprocessor = PreProcessor()
+    preprocessor.read_csv(
         [get_full_path("data/bwr_input.csv"), get_full_path("data/bwr_output.csv")],
     )
+    return preprocessor
 
 
 # Load HTGR micro reactor quadrant power data before preprocessing
 def load_qpower():
-    return PreProcesser(
+    preprocessor = PreProcessor()
+    preprocessor.read_csv(
         get_full_path("data/microreactor.csv"), slice(29, 37), slice(4, 8)
     )
+    return preprocessor
 
 
 # Load HTGR micro-reactor quadrant power data after preprocessing using symmetry conditions
 def load_pqpower():
-    return PreProcesser(
+    preprocessor = PreProcessor()
+    preprocessor.read_csv(
         get_full_path("data/microreactor_preprocessed.csv"), slice(1, 9), slice(9, 14)
     )
+    return preprocessor
--- pyMAISE/preprocessing.py
+++ pyMAISE/preprocessing.py
@@ -1,40 +1,80 @@
+import copy
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import xarray as xr
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 import pyMAISE.settings as settings
 
 
-class PreProcesser:
-    def __init__(
+class PreProcessor:
+    def read_csv(
         self,
         path,
         input_slice: slice = None,
         output_slice: slice = None,
         header="infer",
     ):
+        """
+        Read comma-separated values (csv) file into `xarray.DataArray` for
+        `PreProcessor.data`, `PreProcessor.inputs`, and `PreProcessor.outputs`.
+
+        Parameters
+        ----------
+        path : str or list of str
+            The path to the single input file containing both inputs and outputs
+            or a list of two paths, the first to the inputs and the second to
+            the outputs.
+        input_slice : slice
+            Indice slice of inputs (required if only a single path was given in `path`).
+        output_slice : slice
+            Indice slice of outputs (required if only a single path was given in `path`).
+        header : int, 'infer', or None
+            Row number(s) containing column labels and marking the start of the data.
+            Used in pandas.read_csv.
+        """
+        if settings.values.verbosity > 0:
+            print("Reading data from", path)
+
         if isinstance(path, str):
-            # Read in data from path
-            self._data = pd.read_csv(path, header=header)
             self._input_slice = input_slice
             self._output_slice = output_slice
 
-            # Separate data based on x and y indicies
-            self._inputs = self._data.iloc[:, input_slice]
-            self._outputs = self._data.iloc[:, output_slice]
+            # Read in data from path
+            self._data = (
+                pd.read_csv(path, header=header)
+                .to_xarray()
+                .to_array()
+                .transpose(..., "variable")
+            )
+
+            # Separate data based on slices
+            self._inputs = self._data.isel(variable=self._input_slice)
+            self._outputs = self._data.isel(variable=self._output_slice)
+
         elif isinstance(path, list):
             # Make sure only two paths are given
             assert len(path) == 2
 
             # Assuming the first path is the input and second is
             # output data
-            self._inputs = pd.read_csv(path[0], header=header)
-            self._outputs = pd.read_csv(path[1], header=header)
+            self._inputs = (
+                pd.read_csv(path[0], header=header)
+                .to_xarray()
+                .to_array()
+                .transpose(..., "variable")
+            )
+            self._outputs = (
+                pd.read_csv(path[1], header=header)
+                .to_xarray()
+                .to_array()
+                .transpose(..., "variable")
+            )
 
-            # Join input and output DataFrames
-            self._data = pd.concat([self._inputs, self._outputs], axis=1)
+            # Join input and output
+            self._data = self._inputs.combine_first(self._outputs)
 
             # Fill slicing information
             self._input_slice = slice(0, self._inputs.shape[1])
@@ -42,97 +82,204 @@ class PreProcesser:
                 self._inputs.shape[1] - 1, self._outputs.shape[1]
             )
 
-        # Scalers
-        self._xscaler = None
-        self._yscaler = None
+    def set_data(self, data: xr.DataArray, inputs: list, outputs: list = None):
+        """
+        Set `PreProcessor.data`, `PreProcessor.inputs`, and `PreProcessor.outputs`.
 
-        if settings.values.verbosity > 0:
-            print("Reading data from", path)
+        Parameters
+        ----------
+        data : xarray.DataArray or list of xarray.DataArray
+            Raw data shaped with the feature dimension as the last dimension.
+        inputs : list
+            List of names or indices for each feature to be used in the inputs.
+        outputs : list
+            List of names or indices for each feature to be used in the outputs. If none
+            is given and `inputs` does not include all features in `data` then the
+            outputs are se to the remaining features. If none is given and `inputs`
+            includes all features in `data` then a parallel series is assumed and
+            `outputs = inputs`.
+        """
+        if isinstance(data, xr.DataArray):
+            feature_dim = data.dims[-1]
+            self._data = data.to_dataset(dim=feature_dim)
+            # Restrict input to the features given by the user
+            self._inputs = self._data[inputs]
 
-            if settings.values.verbosity == 2:
-                print(self._data.head(5))
+            # If outputs are given then use them, if no outputs but inputs
+            # has all features in the data set then assume parallel series,
+            # and if none are true then drop the input features from the
+            # data set to create outputs
+            if outputs:
+                self._outputs = self._data[outputs]
+            elif set(self._inputs.keys()) == set(self._data.keys()):
+                self._outputs = self._data[inputs]
+            else:
+                self._outputs = self._data.drop_vars(inputs)
 
-    def data_split(self):
-        return train_test_split(
-            self._inputs,
-            self._outputs,
+        elif isinstance(data, list):
+            self._inputs = data[0]
+            self._outputs = data[1]
+
+            # Join input and output
+            self._data = self._inputs.combine_first(self._outputs)
+
+        else:
+            raise RuntimeError("Data type not supported for data")
+
+        # Ensure the feature dimension is last so numpy arrays are correct
+        self._data = self._data.to_array().transpose(..., feature_dim)
+        self._inputs = self._inputs.to_array().transpose(..., feature_dim)
+        self._outputs = self._outputs.to_array().transpose(..., feature_dim)
+
+    def split_sequences(
+        self,
+        input_steps: int,
+        output_steps: int,
+        output_lookback: int = 1,
+        dim: str = None,
+    ):
+        """
+        Split time series data. Populates input and output data with `xarrays.DataArray`
+        that are dimensions of (samples, time steps, features). These can be accesed
+        through `PreProcessor.inputs` and `PreProcessor.outputs` accessors.
+
+        Parameters
+        ----------
+        input_steps : int
+            The number of time steps for each sample.
+        output_steps : int
+            Number of time steps for each sample.
+        output_lookback : int
+            The number of time steps to subtract from the end index of the inputs.
+            This is default set to 1 but is set to zero for parallel series
+        dim : str
+            The name of the dimension to split the time series. Assumes the first dimension.
+        """
+        # Assert our data has 2-D, the result of splie_sequences will be 3-D
+        assert len(self._data.shape) == 2
+
+        # Alter output_lookback if doing parallel series
+        if (
+            set(self._inputs.coords[self._inputs.dims[-1]].to_numpy())
+            == set(self._outputs.coords[self._outputs.dims[-1]].to_numpy())
+            and output_lookback == 1
+        ):
+            output_lookback = 0
+
+        # Use first dimension if not given
+        if dim == None:
+            dim = self._data.dims[0]
+
+        # Iterate through 2D data to make 3D data for time series
+        x, y = list(), list()
+        for i in range(self._data.coords[dim].shape[0]):
+            input_end_idx = i + input_steps
+            output_end_idx = input_end_idx + output_steps
+            if output_end_idx > self._data.coords[dim].shape[0]:
+                break
+
+            x.append(self._inputs.isel(**{dim: slice(i, input_end_idx)}).to_numpy())
+            y.append(
+                self._outputs.isel(
+                    **{dim: slice(input_end_idx - output_lookback, output_end_idx)}
+                ).to_numpy()
+            )
+        x = np.array(x)
+        y = np.array(y)
+
+        # Convert to xarray.DataArray(s)
+        self._inputs = xr.DataArray(
+            x,
+            coords={
+                "samples": range(x.shape[0]),
+                "sequence_idx": range(x.shape[1]),
+                self._inputs.dims[-1]: self._inputs.coords[
+                    self._inputs.dims[-1]
+                ].to_numpy(),
+            },
+        )
+        self._outputs = xr.DataArray(
+            y,
+            coords={
+                "samples": range(y.shape[0]),
+                "sequence_idx": range(y.shape[1]),
+                self._outputs.dims[-1]: self._outputs.coords[
+                    self._outputs.dims[-1]
+                ].to_numpy(),
+            },
+        )
+
+    def train_test_split(self, scaler=None, scale_x: bool = True, scale_y: bool = True):
+        """
+        Split data into training and testing data sets.
+
+        Parameters
+        ----------
+        scaler : sklearn scaler
+            This an `sklearn.processing` such as `MinMaxScaler()` that scales the input
+            and output data.
+        scale_x : bool
+            If `scaler` should be used on the inputs.
+        scale_y : bool
+            If `scaler` should be used on the outputs.
+        """
+        # Assuming the last dimension is the dimension for features
+        # get the name of that dimension
+        feature_dim_name = self._data.dims[-1]
+
+        # Run sklearn.preprocessing.train_test_split on the coordinates
+        # for the feature_dim_name dimension
+        samples_idx = self._inputs.coords[self._inputs.dims[0]]
+        train_idx, test_idx = train_test_split(
+            samples_idx.to_numpy(),
             test_size=settings.values.test_size,
             random_state=settings.values.random_state,
         )
 
-    def min_max_scale(self, scale_x: bool = True, scale_y: bool = True):
-        # Split data
-        xtrain, xtest, ytrain, ytest = self.data_split()
+        # Split to xtrain, xtest, ytrain, ytest using indices and assuming the
+        # first dimension in the array is the sample dimension
+        self._split_data = (
+            self._inputs.isel(
+                **{self._inputs.dims[0]: train_idx.astype(int)}
+            ).transpose(..., feature_dim_name),
+            self._inputs.isel(**{self._inputs.dims[0]: test_idx.astype(int)}).transpose(
+                ..., feature_dim_name
+            ),
+            self._outputs.isel(
+                **{self._outputs.dims[0]: train_idx.astype(int)}
+            ).transpose(..., feature_dim_name),
+            self._outputs.isel(
+                **{self._outputs.dims[0]: test_idx.astype(int)}
+            ).transpose(..., feature_dim_name),
+        )
 
-        # Scale x
-        if scale_x:
-            self._xscaler = MinMaxScaler()
-            xtrain = pd.DataFrame(
-                self._xscaler.fit_transform(xtrain),
-                index=xtrain.index,
-                columns=xtrain.columns,
-            )
-            xtest = pd.DataFrame(
-                self._xscaler.transform(xtest),
-                index=xtest.index,
-                columns=xtest.columns,
-            )
+        # Scale the data using the sklearn.preprocessing function given by the user
+        if scaler:
+            if scale_x:
+                self._xscaler = copy.deepcopy(scaler)
+                self._split_data[0].values = self._xscaler.fit_transform(
+                    self._split_data[0].values.reshape(
+                        -1, self._split_data[0].values.shape[-1]
+                    )
+                ).reshape(self._split_data[0].values.shape)
+                self._split_data[1].values = self._xscaler.transform(
+                    self._split_data[1].values.reshape(
+                        -1, self._split_data[1].values.shape[-1]
+                    )
+                ).reshape(self._split_data[1].values.shape)
 
-        # Scale y
-        if scale_y:
-            self._yscaler = MinMaxScaler()
-            ytrain = pd.DataFrame(
-                self._yscaler.fit_transform(ytrain),
-                index=ytrain.index,
-                columns=ytrain.columns,
-            )
-            ytest = pd.DataFrame(
-                self._yscaler.transform(ytest),
-                index=ytest.index,
-                columns=ytest.columns,
-            )
-
-        if settings.values.verbosity > 0:
-            print("MinMax scaling data")
-
-        return (xtrain, xtest, ytrain, ytest)
-
-    def std_scale(self, scale_x: bool = True, scale_y: bool = True):
-        # Split data
-        xtrain, xtest, ytrain, ytest = self.data_split()
-
-        # Scale x
-        if scale_x:
-            self._xscaler = StandardScaler()
-            xtrain = pd.DataFrame(
-                self._xscaler.fit_transform(xtrain),
-                index=xtrain.index,
-                columns=xtrain.columns,
-            )
-            xtest = pd.DataFrame(
-                self._xscaler.transform(xtest),
-                index=xtest.index,
-                columns=xtest.columns,
-            )
-
-        # Scale y
-        if scale_y:
-            self._yscaler = StandardScaler()
-            ytrain = pd.DataFrame(
-                self._yscaler.fit_transform(ytrain),
-                index=ytrain.index,
-                columns=ytrain.columns,
-            )
-            ytest = pd.DataFrame(
-                self._yscaler.transform(ytest),
-                index=ytest.index,
-                columns=ytest.columns,
-            )
-
-        if settings.values.verbosity > 0:
-            print("MinMax scaling data")
-
-        return (xtrain, xtest, ytrain, ytest)
+            if scale_y:
+                self._yscaler = copy.deepcopy(scaler)
+                self._split_data[2].values = self._yscaler.fit_transform(
+                    self._split_data[2].values.reshape(
+                        -1, self._split_data[2].values.shape[-1]
+                    )
+                ).reshape(self._split_data[2].values.shape)
+                self._split_data[3].values = self._yscaler.transform(
+                    self._split_data[3].values.reshape(
+                        -1, self._split_data[3].values.shape[-1]
+                    )
+                ).reshape(self._split_data[3].values.shape)
 
     def correlation_matrix(
         self,
@@ -144,7 +291,11 @@ class PreProcesser:
         annotations=False,
     ):
         # Create correlation matrix DataFrame
-        corr = self._data.corr(method, min_periods)
+        corr = (
+            self._data.to_dataset(dim=self._data.dims[-1])
+            .to_dataframe()
+            .corr(method, min_periods)
+        )
 
         # Get figure and axis object if not provided
         if ax == None and fig == None:
@@ -178,16 +329,20 @@ class PreProcesser:
 
     # Getters
     @property
-    def data(self) -> pd.DataFrame:
+    def data(self) -> xr.DataArray:
         return self._data
 
     @property
-    def inputs(self) -> pd.DataFrame:
+    def inputs(self) -> xr.DataArray:
         return self._inputs
 
     @property
-    def outputs(self) -> pd.DataFrame:
+    def outputs(self) -> xr.DataArray:
         return self._outputs
+
+    @property
+    def split_data(self):
+        return self._split_data
 
     @property
     def xscaler(self):
@@ -196,3 +351,16 @@ class PreProcesser:
     @property
     def yscaler(self):
         return self._yscaler
+
+    # Setters
+    @data.setter
+    def data(self, data: xr.DataArray):
+        self._data = data
+
+    @inputs.setter
+    def inputs(self, inputs: xr.DataArray):
+        self._inputs = inputs
+
+    @outputs.setter
+    def outputs(self, outputs: xr.DataArray):
+        self._outputs = outputs
